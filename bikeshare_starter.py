575b103f582146efb0219a63a8818e413dd54bd8
Schmithals, Gerrit (T9-A) <gerrit.schmithals@cariad.technology>
2026-04-14 16:16:06 +0200

    docs: Add comment to Python code explaining context

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -6,6 +6,8 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+# This Python script is only used to demonstrate capabilities in using Git
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
